40679c9cefea61d562819aecdccdb2e66264c525
sagittatius <qiaoliang.email@gmail.com>
2019-11-18 21:08:46 +0800

    add the second test.

--- test/test.cpp
+++ test/test.cpp
@@ -2,37 +2,43 @@
 #include "gmock/gmock.h"
 #include "Person.h"
 #include "demo.h"
+
 namespace {
-    class ClassDeclaration : public testing::Test {
+    class RoverExecutionTest : public testing::Test {
     public:
-        Person obj;
-        ClassDeclaration(){
-            obj;
+        mars_practice::CMarsHandle cMarsHandle;
+        int iXmax, iYmax;
+        int iXinput, iYinput;
+        char cDirectionInput;
+        std::string strOperation;
+        std::vector<mars_practice::STParaGroup> vecUserInput;
+
+        RoverExecutionTest(){
+            cMarsHandle.Init();
+            iXmax = 5, iYmax = 5;               // 最大边界X值/Y值
+            iXinput = 1, iYinput = 1;           // 用户输入位置
+            cDirectionInput = 'N';              // 用户输入方向
+            strOperation = "M";                 // 具体操作内容
         }
     };
 }
 
-TEST_F(ClassDeclaration, getAge){
-    obj.setAge(30);
-    ASSERT_EQ(obj.getAge(),30);
-}
-
-TEST_F(ClassDeclaration, getName){
-    obj.setName("Peter");
-    ASSERT_NE("David",obj.getName());
-    ASSERT_EQ("Peter",obj.getName());
-}
-
-TEST(TestExecution, test_Rover_should_move_one_step_toward_N){
-    mars_practice::CMarsHandle cMarsHandle;
-    cMarsHandle.Init();
-    int iXmax = 5, iYmax = 5;                // 最大边界X值/Y值
-    int iXinput = 1, iYinput = 1;            // 用户输入位置
-    char cDirectionInput = 'N';              // 用户输入方向
-    std::string strOperation = "M";  // 具体操作内容
-    std::vector<mars_practice::STParaGroup> vecUserInput;// 用户输入参数组
+TEST_F(RoverExecutionTest, test_Rover_should_move_one_step_toward_N){
     mars_practice::STParaGroup stUserPara(iXinput, iYinput, cDirectionInput, strOperation);
     vecUserInput.push_back(stUserPara); // 写入用户的多组输入
+
     std::string result=execute(cMarsHandle,iXmax,iYmax,vecUserInput);
+
     ASSERT_EQ("1 2 N\n", result);
+}
+
+TEST_F(RoverExecutionTest, test_GIVEN_rover_toward_N_WHEN_turn_L_THEN_toward_W){
+    cDirectionInput = 'N';              // 用户输入方向
+    strOperation = "L";  // 具体操作内容
+    mars_practice::STParaGroup stUserPara(iXinput, iYinput, cDirectionInput, strOperation);
+    vecUserInput.push_back(stUserPara); // 写入用户的多组输入
+
+    std::string result=execute(cMarsHandle,iXmax,iYmax,vecUserInput);
+
+    ASSERT_EQ("1 1 W\n", result);
 }
